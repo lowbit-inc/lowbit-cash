f8f7a88624e6c0307644af2a4750756e0e4d5ce3
Eric Guimarães de Sousa Silva <eric.gssilva@gmail.com>
2025-11-23 23:00:32 -0300

    Minor changes

--- libs/transaction.sh
+++ libs/transaction.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 function transaction_help() {
-  echo "${system_banner} - Transactions"
+  echo "${system_banner} - Transactions Usage"
   echo
-  echo "  ${system_basename} transaction list [--account ACCOUNT_ID] [--envelope ENVELOPE_ID]"
+  echo "${system_basename} transaction add"
+  echo "  --account ACCOUNT_ID"
+  echo "  --envelope ENVELOPE_ID"
+  echo "  --date DATE"
+  echo "  --amount AMOUNT"
+  echo "  [--description OPTIONAL_TEXT]"
+  echo
+  echo "${system_basename} transaction list"
+  echo "  [--account ACCOUNT_ID]"
+  echo "  [--envelope ENVELOPE_ID]"
   echo
   exit 0
 }
